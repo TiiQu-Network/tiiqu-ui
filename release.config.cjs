e2ef2b34946cdd989517d842c6043a4c94de1451
Proac-Tee <babatundetaiwo038@gmail.com>
2025-07-20 09:09:37 +0100

    fix: github ci/cd workflow build error

--- release.config.cjs
+++ release.config.cjs
@@ -1,3 +1,13 @@
 module.exports = {
   branches: ["main", { name: "next", prerelease: true }],
+  tagFormat: "v${version}",
+  preset: "conventionalcommits",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+  ],
+  changelogFile: "CHANGELOG.md",
 };
