baa3b04a3bb96ef0e8ec903a36ed953c36e0c0ba
Proac-Tee <babatundetaiwo038@gmail.com>
2025-08-23 16:12:07 +0100

    feat(button): make focus visbile

--- src/components/Button/variant.ts
+++ src/components/Button/variant.ts
@@ -1,7 +1,7 @@
 import { cva } from "class-variance-authority";
 
 export const buttonVariant = cva(
-	"inline-flex items-center justify-center gap-1  py-2 cursor-pointer transition-all duration-300 ease-in-out px-4 text-base outline-transparent",
+	"inline-flex items-center justify-center gap-1  py-2 cursor-pointer transition-all duration-300 ease-in-out px-4 text-base",
 	{
 		variants: {
 			intent: {
@@ -34,7 +34,7 @@ export const buttonVariant = cva(
 				true: "opacity-45 cursor-not-allowed",
 			},
 			loading: {
-				true: "pointer-events-none relative",
+				true: "cursor-not-allowed relative",
 				false: "",
 			},
 			iconPosition: {
